f7db01be3eb58d4f7790efce1190e537a34ebc8f
Brandon <58278801+brandonimous@users.noreply.github.com>
2023-03-06 15:51:54 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Posta from "./components/Posta.vue";
+
+import { ref } from "vue";
+
+const posts = ref([]);
+
+fetch('https://pokeapi.co/api/v2/pokemon')
+  .then(res => res.json())
+  .then(data => posts.value = data.results);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h2>Pokémon</h2>
+    <Posta v-for="posta in posts" :key="posta.url" :name="posta.name" :url="posta.url"></Posta>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
