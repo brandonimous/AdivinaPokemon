51bd57a2f925e32cda3d004bb72f86ce64091b8b
Brandon <58278801+brandonimous@users.noreply.github.com>
2023-03-06 15:52:14 -0700

    Create Posta.vue

---
+++ src/components/Posta.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(['name', 'url']);
+</script>
+
+<template>
+    <div class="container">
+        <h3>{{ name }}</h3>
+        <h5>{{ url }}</h5>
+    </div>
+</template>
